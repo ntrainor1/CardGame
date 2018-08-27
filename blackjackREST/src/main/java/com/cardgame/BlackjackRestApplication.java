626dba5ccd53a96b174568d0942ab0698675a95e
Nathaniel Trainor <natetrainor@msn.com>
2018-08-26 19:12:27 -0600

    New refreshed REST

---
+++ blackjackREST/src/main/java/com/cardgame/BlackjackRestApplication.java
@@ -0,0 +1,12 @@
+package com.cardgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackRestApplication.class, args);
+	}
+}
