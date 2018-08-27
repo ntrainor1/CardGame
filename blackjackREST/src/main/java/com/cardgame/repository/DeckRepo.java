63718047bccad5e014488f5253773f3cf419e04d
Nathaniel Trainor <natetrainor@msn.com>
2018-08-26 18:41:07 -0600

    Scrapping and refreshing REST

--- blackjackREST/src/main/java/com/cardgame/repository/DeckRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cardgame.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cardgame.entities.Deck;
-
-public interface DeckRepo extends JpaRepository<Deck, Integer> {
-
-}
